0eb80ff5badaff01462c38b4fcd837dfd6a55098
William Capozzoli <thinkocapo@gmail.com>
2020-04-12 11:44:16 -0700

    sqlite blank

---
+++ sqlite-test.py
@@ -0,0 +1,44 @@
+from gzip import GzipFile
+import json
+from six import BytesIO
+import sqlite3
+
+# Functions from getsentry/sentry-python
+def decompress_gzip(bytes_encoded_data):
+    try:
+        fp = BytesIO(bytes_encoded_data)
+        try:
+            f = GzipFile(fileobj=fp)
+            return f.read().decode("utf-8")
+        finally:
+            f.close()
+    except Exception as e:
+        raise e
+
+# if path is outside of directory, must use absolute path like /home/user/database.db
+path_to_database = r"sqlite.db"
+conn = sqlite3.connect(path_to_database)
+
+try:
+    with conn:
+        cur = conn.cursor()
+        cur.execute("SELECT * FROM events")
+
+        rows = cur.fetchall()
+        print('TOTAL ROWS: ', len(rows))
+
+        test = rows[-1]
+        test = list(test)
+        print('ID OF LATEST ROW', test[0])
+
+        # <read-write buffer ptr 0x562a8e765e30, size 1522 at 0x562a8e765df0>
+        # <type 'buffer'>
+        buffer = test[3]
+
+        #  Print a Sample
+        # print('type(buffer)', type(buffer))
+        # json_body = decompress_gzip(str(buffer))
+        # dict_body = json.loads(json_body)
+        # print('dict_body', dict_body)
+except Exception as e:
+    print('EXCEPTION', e)
