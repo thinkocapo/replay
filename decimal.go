86a5c9249fb8114ed4196ef121683ef5682d1161
William Capozzoli <thinkocapo@gmail.com>
2020-06-05 22:04:51 -0700

    sentry timestamp format

--- decimal.go
+++ decimal.go
@@ -7,6 +7,8 @@ import (
 	"time"
 )
 
+// https://github.com/getsentry/sentry-javascript/tree/master/packages/utils
+// https://github.com/getsentry/sentry-javascript/blob/c6a2ec95f5c21df5fb6c4d7ee07087b615e23436/packages/utils/src/misc.ts
 func main() {
 	// price, err := decimal.NewFromString("136.02")
 	// if err != nil {
@@ -30,6 +32,7 @@ func main() {
 	// fmt.Println("Total:", total)                            // Total: 459.824961375
 	// fmt.Println("Tax rate:", total.Sub(preTax).Div(preTax)) // Tax rate: 0.08875
 
+	// try with 1591051102.7653 as well
 	startTimestamp, _ := decimal.NewFromString("1591051102.765368")
 	endTimestamp, _ := decimal.NewFromString("1591051102.777408")
 
@@ -39,16 +42,14 @@ func main() {
 	// timestamp, _ := decimal.NewFromString(string(time.Now().Unix()))
 	// timestamp := decimal.NewFromFloat(float64(time.Now().Unix()))
 	timestamp := time.Now().UnixNano()
-
 	timestamp1 := fmt.Sprint(timestamp)
 
 	// timestamp = strconv.FormatInt(timestamp, 10)
 	// fmt.Println("\ntstring:", timestamp)
 
-	ts := timestamp1[:10] + "." + timestamp1[11:]
-	// https://github.com/getsentry/sentry-javascript/tree/master/packages/utils
-	// https://github.com/getsentry/sentry-javascript/blob/c6a2ec95f5c21df5fb6c4d7ee07087b615e23436/packages/utils/src/misc.ts
-	fmt.Println("\ntimestamp:", ts)
+	fmt.Println("\ntimestamp timestamp1:", timestamp1)
+	sentryTimestamp := timestamp1[:11] + "." + timestamp1[11:]
+	fmt.Println("\ntimestamp         sentryTimestamp:", sentryTimestamp)
 
 	timestamp1000 := time.Now().UnixNano()
 	base := timestamp1000 / 1000000
@@ -56,14 +57,12 @@ func main() {
 	fmt.Println("\ntimestamp1000 base:", base)
 	fmt.Println("\ntimestamp1000 modulo:", modulo)
 
-
 	// myv := float64(ts) // fails
 
 	// newStartTimestamp := timestamp
 	// newEndTimestamp := timestamp.Add(difference)
 	// fmt.Println("\nnewStartTimestamp:", newStartTimestamp)
 	// fmt.Println("\nnewEndTimestamp:", newEndTimestamp)
-
 
 	// now1 := time.Now()
 	// fmt.Print("time.Now().UnixNano()", now1.UnixNano())
