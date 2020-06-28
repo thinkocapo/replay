87de442867c8711fc1ae501be403331ed9fd31d6
William Capozzoli <thinkocapo@gmail.com>
2020-06-27 22:09:28 -0700

    Go Module. timestamps moved to file. Instructions bin

--- deprecated/go/dump-request.go
+++ deprecated/go/dump-request.go
@@ -1,47 +1,37 @@
 
-package main
+// package main
 
-import (
-	"fmt"
-	"github.com/gorilla/mux"
-	"log"
-	"net/http"
-	"encoding/json"
-)
+// import (
+// 	"fmt"
+// 	"github.com/gorilla/mux"
+// 	"log"
+// 	"net/http"
+// 	"encoding/json"
+// )
 
-type Event struct {
-	Platform string
-	Level string
-	Server_name string
-}
+// type Event struct {
+// 	Platform string
+// 	Level string
+// 	Server_name string
+// }
 
+// func DecodeRequest(w http.ResponseWriter, req *http.Request) {
 
-func DecodeRequest(w http.ResponseWriter, req *http.Request) {
+// 	var event Event
+// 	err := json.NewDecoder(req.Body).Decode(&event)
+// 	if err != nil {
+// 		fmt.Printf("%v", "\n------- ERRROR --------\n")
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	} else {
+// 		fmt.Printf("%v", event) // logs Platform, Level, Server_name as platform, level, server_name
+// 	}
 
-	// requestDump, err := httputil.DumpRequest(req, true)
-	// fmt.Print(string(requestDump))
-	// sends as the http response
-	// fmt.Fprint(w, string(requestDump))
-	// if err != nil {
-	// 	fmt.Fprint(w, err.Error())
-	// } else {
-	// }
+// }
 
-	var event Event
-	err := json.NewDecoder(req.Body).Decode(&event)
-	if err != nil {
-		fmt.Printf("%v", "\n------- ERRROR --------\n")
-		http.Error(w, err.Error(), http.StatusBadRequest)
-		return
-	} else {
-		fmt.Printf("%v", event) // logs Platform, Level, Server_name as platform, level, server_name
-	}
-
-}
-
-func main() {
-	router := mux.NewRouter()
-	router.HandleFunc("/decoderequest", DecodeRequest).Methods("GET")
-	router.HandleFunc("/decoderequest", DecodeRequest).Methods("POST")
-	log.Fatal(http.ListenAndServe(":12345", router))
-}
+// func main() {
+// 	router := mux.NewRouter()
+// 	router.HandleFunc("/decoderequest", DecodeRequest).Methods("GET")
+// 	router.HandleFunc("/decoderequest", DecodeRequest).Methods("POST")
+// 	log.Fatal(http.ListenAndServe(":12345", router))
+// }
---
+++ go.mod
@@ -0,0 +1,12 @@
+module undertaker
+
+go 1.12
+
+require (
+	github.com/buger/goreplay v1.0.0
+	github.com/buger/jsonparser v1.0.0
+	github.com/google/uuid v1.1.1
+	github.com/joho/godotenv v1.3.0
+	github.com/mattn/go-sqlite3 v2.0.3+incompatible
+	github.com/shopspring/decimal v1.2.0
+)
