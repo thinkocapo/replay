7e0486c665416854e175c6d64c3f4ea6b0b7a01b
William Capozzoli <thinkocapo@gmail.com>
2020-06-27 20:15:23 -0700

    db commands

---
+++ test/remove-db.py
@@ -0,0 +1,14 @@
+
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+database = os.getenv('SQLITE')
+
+decision = input("> Remove {} are you sure? y/n ".format(database))
+
+if decision == "y":
+    #os.remove(database)
+    print('database cleared')
+else:
+    print('nothing happened')
