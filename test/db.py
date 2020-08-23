4dade80d716aab6249743b97fc716d8fe59490c6
William Capozzoli <thinkocapo@gmail.com>
2020-08-23 15:16:06 -0700

    test/db.py for json works

--- test/db.py
+++ test/db.py
@@ -25,50 +25,47 @@ def decompress_gzip(bytes_encoded_data):
     except Exception as e:
         raise e
 
-JSON = os.getenv('JSON')
-database = JSON or os.getcwd() + "/db.json"
-# print(database)
+database = os.getenv('JSON') or os.getcwd() + "/db.json"
 
 try:
-    with open(JSON) as file:
+    with open(database) as file:
 
         events = json.load(file)
-        print(len(events))
+        event = {}
 
         _body = sys.argv[2] if len(sys.argv) > 2 else None
         _id = sys.argv[1] if len(sys.argv) > 1 else None
         
-        # print(events[1])       # works 
         if _id==None:
-            events    
-            print('TOTAL ROWS ALL: ', len(events))
+            events
+            event = events[0]    
+            print('len(events)', len(events))
         else:
-            events2 = events[int(_id)]
-            print('TOTAL ROWS _id: ', events2)
+            event = events[int(_id)]
+            print('selecting 1 event...')
 
-        # row = rows[-1]        
+        sqlite_id = _id
+        event_platform = event['platform']
+        event_type = event['kind']
+        body = event['body']
+        headers = event['headers']
 
-        # sqlite_id = row[0]
-        # event_platform = row[1]
-        # event_type = row[2]
-        # body = row[3] # buffer
-        # headers = row[4]
+        output = {
+            'id': _id,
+            'platform': event['platform'],
+            'type': event['kind'],
+            'headers': event['headers']
+            # 'headers': json.loads(headers)
+        }
 
-        # output = {
-        #     'id': sqlite_id,
-        #     'platform': event_platform,
-        #     'type': event_type,
-        #     'headers': json.loads(headers)
-        # }
-
-        # if _body == '-b':
-        #     print('_body', _body)
-        #     try:
-        #         output['body'] = json.loads(body)
-        #     except:
-        #         output['body'] = body
+        if _body == '-b':
+            print('_body', _body)
+            try:
+                output['body'] = json.loads(body)
+            except:
+                output['body'] = body
         
-        # print(json.dumps(output, indent=2))
+        print(json.dumps(output, indent=2))
     
 except Exception as e:
     print('EXCEPTION in test/db.py:', e)
