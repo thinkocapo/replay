5606409faaddbd9767319cfad9b586c8f828a301
William Capozzoli <thinkocapo@gmail.com>
2020-11-26 15:48:04 -0500

    Counter fixed, New Language Support

--- requests.go
+++ requests.go
@@ -20,25 +20,37 @@ type Requests struct {
 // Doing each destination one-by-one, gives each org a rest before its API is called again, so don't insert a short Sleep Timeout yet
 func (r *Requests) send() {
 	for _, event := range r.events {
-		// fmt.Println("EVENT PLATFORM", event.Platform)
+		// fmt.Println("\nEVENT PLATFORM", event.Platform)
 
+		// CONSIDER check if Destinations array is empty, or do during an init somewhere
 		switch event.Platform {
 		case JAVASCRIPT:
 			for _, dsn := range config.Destinations.Javascript {
 				event.setDsn(dsn)
 				request := NewRequest(event)
 				request.send()
+				counter++
 			}
 		case PYTHON:
 			for _, dsn := range config.Destinations.Python {
 				event.setDsn(dsn)
 				request := NewRequest(event)
 				request.send()
+				counter++
 			}
+		case JAVA:
+			for _, dsn := range config.Destinations.Java {
+				event.setDsn(dsn)
+				request := NewRequest(event)
+				request.send()
+				counter++
+			}
+		default:
+			sentry.CaptureMessage("unsupported event platform: " + event.Platform)
+			fmt.Printf("unrecognized Platform %v", event.Platform)
 		}
-		counter++
 	}
-	fmt.Printf("\n> DONE sending %v events", counter)
+	fmt.Printf("\n> TOTAL sent: %v", counter)
 
 	// does not Capture, not sure why
 	sentry.CaptureMessage("finished sending all requests")
