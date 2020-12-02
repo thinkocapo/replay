b740d912afe8b01e1a341e918df74097e021b5de
William Capozzoli <thinkocapo@gmail.com>
2020-12-02 10:17:02 -0600

    update

--- requests.go
+++ requests.go
@@ -16,8 +16,6 @@ type Requests struct {
 // Doing each destination one-by-one, gives each org a rest before its API is called again, so don't insert a short Sleep Timeout yet
 func (r *Requests) send() {
 	for _, event := range r.events {
-		// fmt.Println("\nEVENT PLATFORM", event.Platform)
-
 		// CONSIDER check if Destinations array is empty, or do during an init somewhere
 		switch event.Platform {
 		case JAVASCRIPT:
