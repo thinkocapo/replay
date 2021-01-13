885de09f60387ebe35d2472db525f3fa7ec89345
William Capozzoli <thinkocapo@gmail.com>
2021-01-13 16:06:16 -0500

    todo for demo-automation tags array

--- event.go
+++ event.go
@@ -158,6 +158,7 @@ func (e Event) undertake() {
 		if e.Error.Tags == nil {
 			e.Error.Tags = make([][]string, 0)
 		}
+		// TODO need to replace the 'demo-automation' element in the array if it exists already, or else you're adding duplicate elements
 		tagItem := []string{"demo-automation", "replay"}
 		e.Error.Tags = append(e.Error.Tags, tagItem)
 	}
