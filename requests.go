4a04d11846b41610eb9cbcc447fe1cb25262f30f
William Capozzoli <thinkocapo@gmail.com>
2021-01-20 15:24:22 -0500

    eval config destinations check

--- requests.go
+++ requests.go
@@ -10,10 +10,10 @@ type Requests struct {
 	events []Event
 }
 
+// TODO in each case met, should check len(config.Destinations.Platform) != 0
 // Doing each destination one-by-one, gives each org a rest before its API is called again, so don't insert a short Sleep Timeout yet
 func (r *Requests) send() {
 	for _, event := range r.events {
-		// EVAL check if Destinations array is empty, or do during an init somewhere
 		switch event.Platform {
 		case JAVASCRIPT:
 			for _, dsn := range config.Destinations.Javascript {
